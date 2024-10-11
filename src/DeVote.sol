48dce1c8dd48532f26ade55671a495d59c738578
sohamkumar <sohamkumar17072003srp@gmail.com>
2024-10-11 21:22:00 +0530

    started creating the overview of the voting contract functionlity and setting up dependencies

--- src/DeVote.sol
+++ src/DeVote.sol
@@ -2,10 +2,36 @@
 
 pragma solidity 0.8.26;
 
-contract DeVote {
+import {MerkleProof} from "@openzeppelin/utils/cryptography/MerkleProof.sol";
+import {Ownable} from "@openzeppelin/access/Ownable.sol";
 
-    constructor() {
-        
+contract DeVote is Ownable {
+    using MerkleProof for bytes32;
+
+    struct Candidate {
+        int256 id;
+        string name;
+        string slogan;
+        int256 voteCount;
     }
 
+    bytes32 private immutable merkleRoot;
+    uint256 private immutable votingPeriod;
+    uint256 private candidateCount;
+    Candidate[] private candidates;
+    mapping(uint256 => Candidate) internal candidateMap;
+
+    constructor(bytes32 _merkleRoot, uint256 _votingPeriod) Ownable(msg.sender) {
+        merkleRoot = _merkleRoot;
+        votingPeriod = _votingPeriod;
+        candidateCount = 0;
+    }
+
+    function startVoting() public onlyOwner {}
+
+    function endVoting() public onlyOwner {}
+
+    function addCandidate() public onlyOwner {}
+
+    function voteCandidate() public returns (bool) {}
 }
