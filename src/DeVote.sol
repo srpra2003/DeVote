6ad389832c410f45968163c28879902c0f298bc6
sohamkumar <sohamkumar17072003srp@gmail.com>
2024-10-11 22:21:58 +0530

    votercasting Function is Finished also finished winner declaration code

--- src/DeVote.sol
+++ src/DeVote.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier : MIT
+//SPDX-License-Identifier:MIT
 
 pragma solidity 0.8.26;
 
@@ -13,6 +13,10 @@ contract DeVote is Ownable {
     error VotingPeriodIsNotFinished();
     error VotingFinished();
     error CandidateWithThisSloganAlreadyAdded(string slogan);
+    error InvalidVoterRight(address);
+    error InvalidCandidateId();
+    error AlreadyVoted(bytes32 voterRightHAsh);
+    error NoVotesCastedHenceVotingFailed();
 
     struct Candidate {
         uint256 id;
@@ -24,20 +28,31 @@ contract DeVote is Ownable {
     }
 
     event NewCandidateAdded(uint256 indexed id, string name, string slogan);
+    event VoteDeposited(bytes32 indexed voterRightHash);
+    event WinnerCandidDeclared(uint256 candidId);
 
     bytes32 private immutable merkleRoot;
     uint256 private immutable votingPeriod;
     uint256 private votingStartTime;
     bool private votingFinished;
     uint256 private candidateCount;
+    Candidate private winnerCandid;
     Candidate[] private candidates;
     mapping(bytes32 sloganHash => bool) internal isCandidateAdded; //each unique slogan will represent unique candidate
+    mapping(bytes32 voterRightHash => bool hasVoted) internal hasVoterVoted;
 
     modifier VotingOpen() {
         if (votingStartTime == 0) {
             revert VotingNotStarted();
         } else if (votingFinished) {
             revert VotingFinished();
+        }
+        _;
+    }
+
+    modifier ValidCadidateId(uint256 candidIdToVote) {
+        if (candidIdToVote >= candidateCount) {
+            revert InvalidCandidateId();
         }
         _;
     }
@@ -56,11 +71,26 @@ contract DeVote is Ownable {
         votingStartTime = block.timestamp;
     }
 
-    function endVoting() public VotingOpen onlyOwner {
+    function endVoting() public VotingOpen {
         if (block.timestamp - votingStartTime < votingPeriod) {
             revert VotingPeriodIsNotFinished();
         }
         votingFinished = true;
+
+        uint256 maxVotes = 0;
+        for (uint256 i = 0; i < candidateCount; i++) {
+            Candidate memory candid = candidates[i];
+            if (maxVotes < candid.voteCount) {
+                maxVotes = candid.voteCount;
+                winnerCandid = candid;
+            }
+        }
+
+        if (maxVotes == 0) {
+            revert NoVotesCastedHenceVotingFailed();
+        }
+
+        emit WinnerCandidDeclared(winnerCandid.id);
     }
 
     function addCandidate(string memory _cName, string memory _cSlogan) public onlyOwner {
@@ -84,5 +114,33 @@ contract DeVote is Ownable {
         emit NewCandidateAdded(newCandid.id, _cName, _cSlogan);
     }
 
-    function voteCandidate() public returns (bool) {}
+    function voteCandidate(bytes32[] memory _proof, bytes32 voterRightHash, uint256 candidIdToVote)
+        public
+        ValidCadidateId(candidIdToVote)
+        returns (bool)
+    {
+        if (!MerkleProof.verify(_proof, merkleRoot, voterRightHash)) {
+            revert InvalidVoterRight(msg.sender);
+        }
+        if (hasVoterVoted[voterRightHash]) {
+            revert AlreadyVoted(voterRightHash);
+        }
+
+        candidates[candidIdToVote].voteCount++;
+        emit VoteDeposited(voterRightHash);
+        return true;
+    }
+
+    function getCandidInformation(uint256 candidId)
+        public
+        view
+        ValidCadidateId(candidId)
+        returns (string memory, string memory, uint256)
+    {
+        return (candidates[candidId].name, candidates[candidId].slogan, candidates[candidId].voteCount);
+    }
+
+    function getListOfCandidates() public view returns (Candidate[] memory) {
+        return candidates;
+    }
 }
