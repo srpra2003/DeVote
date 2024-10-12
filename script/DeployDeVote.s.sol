038e207e39a7e7dfac173ffa910ba095067aba48
sohamkumar <sohamkumar17072003srp@gmail.com>
2024-10-12 10:56:24 +0530

    finished writing deployment script

--- script/DeployDeVote.s.sol
+++ script/DeployDeVote.s.sol
@@ -2,9 +2,28 @@
 
 pragma solidity ^0.8.26;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {DeVote} from "../src/DeVote.sol";
+import {stdJson} from "forge-std/StdJson.sol";
 
 contract DeployDeVote is Script {
-    function run() external returns (DeVote) {}
+    using stdJson for string;
+
+    uint256 private constant VOTING_PERIOD = 2 days;
+
+
+    function run() external returns (DeVote) {
+
+        uint256 deployerKey = (block.chainid == 31337) ? (vm.envUint("ANVIL_PRIVATE_KEY")) : (vm.envUint("SEPOLIA_PRIVATE_KEY"));
+        string memory result = vm.readFile(string.concat(vm.projectRoot(),"/script/target/output.json"));
+        bytes32 merkleRoot = result.readBytes32("[0].root");
+
+        console.logBytes32(merkleRoot);
+
+        vm.startBroadcast(deployerKey);
+        DeVote deVote = new DeVote(merkleRoot,VOTING_PERIOD);
+        vm.stopBroadcast();
+
+        return deVote;        
+    }
 }
